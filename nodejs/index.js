08ba65860efba8c84c44451a98ad455592cd403d
ikuratakashi <ikura1108@icloud.com>
2018-07-06 22:13:17 +0900

    a

--- nodejs/index.js
+++ nodejs/index.js
@@ -78,6 +78,9 @@ app.use(validator());
 var cors = require('cors');
 app.use(cors());
 
+//socket.io
+var io = require('socket.io');
+
 //共通設定
 naoetu.ini = {
     ConPool   : "pool",
@@ -459,6 +462,14 @@ naoetu.clsGps.prototype = {
 //サーバデプロイ
 //
 //========================================================================
-app.listen(50001,() => {
+
+//main
+var http = require('http').Server(app);
+
+//socket.io
+var io = require('socket.io')(http);
+
+//サーバ起動
+http.listen(50001,() => {
     naoetu.log.out(3,'Start server port:50001')
 })
