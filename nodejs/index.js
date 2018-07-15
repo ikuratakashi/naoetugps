09f4da7a89406312e0864f4860f2a53655306885
ikuratakashi <ikura1108@icloud.com>
2018-07-16 07:14:41 +0900

    a

--- nodejs/index.js
+++ nodejs/index.js
@@ -737,8 +737,8 @@ naoetu.socket.Response.prototype = {
 naoetu.log.out(1,'SSL Server Deproy Start...');
 var https = require('https');
 var fs = require('fs');
-var ssl_server_key = '../../ssl/server.key';
-var ssl_server_crt = '../../ssl/server.crt';
+var ssl_server_key = '../../ssl/server.key2';
+var ssl_server_crt = '../../ssl/server.crt2';
 var options = {
         key: fs.readFileSync(ssl_server_key),
         cert: fs.readFileSync(ssl_server_crt)
