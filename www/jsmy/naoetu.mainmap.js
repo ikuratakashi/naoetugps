e06c1aa4c4cd4ad3300efa82ece8d455bafcbd76
ikuratakashi <ikura1108@icloud.com>
2018-08-05 05:19:25 +0900

    a

---
+++ www/jsmy/naoetu.mainmap.js
@@ -0,0 +1,3 @@
+//                             pIsSend,pIsViewer,pDefLat,pDefLng,pDefZoom,pMapName,pOutNameLat,pOutNameLng,pTypeListName,pSendBtnName,pResultName,pMapPanMode(cooperative：二本指/greedy:一本指)
+//地図表示側
+NaoetuMain.MapAdd(new naoetu.map(false,true,false,false,15,"mapviewer-map","","","TypeListViewer","DataGetBtn","","cooperative"));
---
+++ www/jsmy/naoetu.sendmap.js
@@ -0,0 +1,4 @@
+//                             pIsSend,pIsViewer,pDefLat,pDefLng,pDefZoom,pMapName,pOutNameLat,pOutNameLng,pTypeListName,pSendBtnName,pResultName,pMapPanMode(cooperative：二本指/greedy:一本指)
+//座標送信側
+NaoetuMain.MapAdd(new naoetu.map(true,false,false,false,18,"mapsend-map","posLat","posLng","TypeList","SendBtn","result1","greedy"));
+
