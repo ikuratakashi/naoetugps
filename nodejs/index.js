85a7dcaa57c660732f5718c54741b471c053a30c
ikuratakashi <ikura1108@icloud.com>
2018-09-12 22:45:09 +0900

    a

--- nodejs/index.js
+++ nodejs/index.js
@@ -777,8 +777,19 @@ io.use(function(socket, next) {
     });
 });
 
+//socketクライアント
+var iocl = require('socket.io-client');
+var socketcl = false;
+
 server.listen(50001,() => {
+
     naoetu.log.out(3,'Start https server port:50001');
+
+    socketcl = iocl.connect('https://arukisoft.com:50001/naoetugps');
+    socketcl.on("connection",function(pSocket){
+        naoetu.log.out(3,'socket.io-client - connection OK!!!!!!!');
+    });
+
 });
 
 //名前空間
@@ -791,20 +802,11 @@ naoetu.log.out(3,'socket.io require');
 //
 //========================================================================
 
-//socketクライアント
-var iocl = require('socket.io-client');
-var socketcl = false;
-
 //---------------------------------------------
 // socket.io コネクション
 //---------------------------------------------
 naoetu.log.out(3,'socket.io routeing "connection" on start...');
 IoNaoetuGps.on("connection",function(pSocket){
-
-    socketcl = iocl.connect('https://arukisoft.com:50001/naoetugps');
-    socketcl.on("connection",function(pSocket){
-        naoetu.log.out(3,'socket.io-client - connection OK!!!!!!!');
-    });
 
     naoetu.log.line(3);
     naoetu.socket.socketObj = pSocket;
