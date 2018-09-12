21fd4bd3ea02f45cec581406ade927068f9ef9fb
ikuratakashi <ikura1108@icloud.com>
2018-09-12 23:25:58 +0900

    a

--- nodejs/index.js
+++ nodejs/index.js
@@ -779,8 +779,8 @@ io.use(function(socket, next) {
 
 //socketクライアント
 var socketcl = require('socket.io-client');
+socketcl.adapter(redis({ host: 'localhost', port: 6379 }));
 var socl = socketcl.connect('https://arukisoft.com:50001');
-socl.adapter(redis({ host: 'localhost', port: 6379 }));
 socl.on("connect",function(pSocket){
     naoetu.log.out(3,'socket.io-client - connection OK!!!!!!!');
 });
