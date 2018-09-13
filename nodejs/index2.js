757adf3a589e82c9ff4888256a30f235bd336139
ikuratakashi <ikura1108@icloud.com>
2018-09-13 23:21:35 +0900

    a

--- nodejs/index2.js
+++ nodejs/index2.js
@@ -3,6 +3,7 @@ var net = require('net');
 var cl = new net.Socket();
 cl.setEncoding('utf8');
 cl.connect('50001', 'localhost', function(){
+    console.log('client-> connected to server');
     cl.write('Who needs a browser to communicate?');
 });
 process.stdin.resume();
