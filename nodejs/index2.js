39fe46ad8323603b606ca173a6495ec973b7018e
ikuratakashi <ikura1108@icloud.com>
2018-09-14 07:33:47 +0900

    a

--- nodejs/index2.js
+++ nodejs/index2.js
@@ -19,7 +19,7 @@ console.log('listening on port 3000');
 //socketクライアント
 var cl = new net.Socket();
 cl.setEncoding('utf8');
-cl.connect('50001',"https://arukisoft.com", function(){
+cl.connect('50001',"loclhost", function(){
     console.log('client-> connected to server');
     //socket.emit("gpswrite","aaaaa");
 
