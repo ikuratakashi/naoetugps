fee09c7badba68b4997605e91cd39fbdbac81158
ikuratakashi <ikura1108@icloud.com>
2018-09-13 22:05:06 +0900

    a

--- nodejs/index2.js
+++ nodejs/index2.js
@@ -1,7 +1,7 @@
 var _io = require('socket.io-client');
 
 var _url = "https://arukisoft.com:50001/naoetugps";
-var _so = _io.connect(_url);
+var _so = _io.connect(_url,{secure: true, reconnect: true});
 _so.on('connect', function (pData){
     naoetu.log.out(3,'io-client  :  connect ' + pData);
     IoNaoetuGps.emit('gpswrite finish',{msg:"naoetu.GpsWrite emit"});
