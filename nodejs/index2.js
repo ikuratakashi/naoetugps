f66ae7a380391855c1955ecb3c5d7a4fbc608333
ikuratakashi <ikura1108@icloud.com>
2018-09-13 23:24:42 +0900

    a

--- nodejs/index2.js
+++ nodejs/index2.js
@@ -1,8 +1,23 @@
-//socketクライアント
 var net = require('net');
+
+var server = net.createServer(function(conn){
+  console.log('server-> tcp server created');
+
+  conn.on('data', function(data){
+    console.log('server-> ' + data + ' from ' + conn.remoteAddress + ':' + conn.remotePort);
+    conn.write('server -> Repeating: ' + data);
+  });
+  conn.on('close', function(){
+    console.log('server-> client closed connection');
+  });
+}).listen(3000);
+
+console.log('listening on port 3000');
+
+//socketクライアント
 var cl = new net.Socket();
 cl.setEncoding('utf8');
-cl.connect('50001', 'localhost', function(){
+cl.connect('3000', 'localhost', function(){
     console.log('client-> connected to server');
     cl.write('Who needs a browser to communicate?');
 });
