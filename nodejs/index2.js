60d3ba9b6aa39836b88c810d4323eceeba669268
ikuratakashi <ikura1108@icloud.com>
2018-09-14 12:41:54 +0900

    Update index2.js

--- nodejs/index2.js
+++ nodejs/index2.js
@@ -27,7 +27,7 @@ cl.on('close', function(){
 });
 */
 var io = require('socket.io-client');
-var socket = io.connect('localhost:50001');
+var socket = io.connect('http://localhost:50001');
  
 socket.on('connect',function(){
   console.log("connect!!");
