643b433cf28142ff609b38a238dc5977362489c7
ikuratakashi <ikura1108@icloud.com>
2018-09-15 06:52:16 +0900

    a

--- nodejs/hello.js
+++ nodejs/hello.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+
+//クロスサイト制限を外す設定
+var cors = require('cors');
+app.use(cors());
 app.get('/', function(req, res) {
 	res.render('index.ejs', {
 		layout : false,
@@ -8,9 +12,28 @@ app.get('/', function(req, res) {
 		}
 	});
 });
-app.listen(3000);
-console.log('Server running at http://127.0.0.1:3000/');
-var io = require('socket.io').listen(app);
+
+//SSL サーバ起動
+var https = require('https');
+var fs = require('fs');
+var ssl_server_key = '../../ssl/server.key';
+var ssl_server_crt = '../../ssl/server.crt';
+var options = {
+        key: fs.readFileSync(ssl_server_key),
+        cert: fs.readFileSync(ssl_server_crt),
+        passphrase: process.env.HTTPS_PASS
+};
+var server = https.createServer(options, app);
+server.listen(3000,function(){
+    naoetu.log.out(3,'Start https server port:50001');
+});
+
+//socket.io作成（redis込み）
+var socketIo = require('socket.io')();
+var io = socketIo.listen(server);
+
+console.log('Server running at https://127.0.0.1:3000/');
+
 io.sockets.on('connection', function(client) {
 	client.on('message', function(msg) {
 		client.send(msg);
