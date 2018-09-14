b49b5981a906bde4c72a553245e501b312608692
ikuratakashi <ikura1108@icloud.com>
2018-09-14 12:39:50 +0900

    Update index2.js

--- nodejs/index2.js
+++ nodejs/index2.js
@@ -27,7 +27,7 @@ cl.on('close', function(){
 });
 */
 var io = require('socket.io-client');
-var socket = io.connect('localhost:8080/naoetugps');
+var socket = io.connect('localhost:8080');
  
 socket.on('connect',function(){
   console.log("connect!!");
