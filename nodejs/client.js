f4c62e0c7efb7d2731a327db4541c5198b4272b0
ikuratakashi <ikura1108@icloud.com>
2018-09-15 07:24:37 +0900

    a

--- nodejs/client.js
+++ nodejs/client.js
@@ -1,5 +1,5 @@
 var client = require('socket.io-client');
-var socket = client.connect('https://localhost:3000');
+var socket = client.connect('localhost:3000');
 socket.on('connect',function(){
     console.log('yea!!');
     socket.send('how are you?');
