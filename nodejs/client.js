60ab057ea985e94a9949f29fdf2249d27a77f5a2
ikuratakashi <ikura1108@icloud.com>
2018-09-15 12:19:32 +0900

    a

--- nodejs/client.js
+++ nodejs/client.js
@@ -1,5 +1,19 @@
+
+
+//設定ファイルの読み込み
+require('dotenv').config();
+
+var fs = require('fs');
+var ssl_server_key = '../../ssl/server.key';
+var ssl_server_crt = '../../ssl/server.crt';
+var options = {
+        key: fs.readFileSync(ssl_server_key),
+        cert: fs.readFileSync(ssl_server_crt),
+        passphrase: process.env.HTTPS_PASS
+};
+
 var client = require('socket.io-client');
-var socket = client.connect('https://arukisoft.com:3000');
+var socket = client.connect('https://arukisoft.com:3000',options);
 socket.on('connect',function(){
     console.log('yea!!');
     socket.send('how are you?');
