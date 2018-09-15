29606c303f73d37fe09ced46a0930019cae50bf0
ikuratakashi <ikura1108@icloud.com>
2018-09-16 08:49:50 +0900

    a

--- nodejs/client.js
+++ nodejs/client.js
@@ -1,5 +1,3 @@
-
-
 //設定ファイルの読み込み
 require('dotenv').config();
 
@@ -8,14 +6,17 @@ var ssl_server_key = '../../ssl/new_server.key';
 var ssl_server_crt = '../../ssl/server.crt';
 var options = {
     key: fs.readFileSync(ssl_server_key),
-//    cert: fs.readFileSync(ssl_server_crt),
+    cert: fs.readFileSync(ssl_server_crt),
     pfx: fs.readFileSync(ssl_server_crt),
     passphrase: process.env.HTTPS_PASS
 };
 
+//socket.io-clientでサーバへ接続
 var client = require('socket.io-client');
-var socket = client.connect('https://localhost',options);
+var socket = client.connect('https://localhost:30000',options);
 
+//処理
+//connectしたら'how are you?'とメッセージを送信する
 socket.on('connect',function(){
     console.log('yea!!');
     socket.send('how are you?');
