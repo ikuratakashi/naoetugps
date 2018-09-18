5a561dfb0485500dbc090f86fe7676f2b01ba077
ikuratakashi <ikura1108@icloud.com>
2018-09-18 22:41:50 +0900

    a

--- nodejs/server2.js
+++ nodejs/server2.js
@@ -1,15 +1,39 @@
 var net = require('net');
+var client = require('socket.io-client');
 
 var server = net.createServer(function(conn){
-  console.log('server-> tcp server created');
 
-  conn.on('data', function(data){
-    console.log('server-> ' + data + ' from ' + conn.remoteAddress + ':' + conn.remotePort);
-    conn.write('server -> Repeating: ' + data);
-  });
-  conn.on('close', function(){
-    console.log('server-> client closed connection');
-  });
-}).listen(30000);
+    console.log('server-> tcp server created');
 
-console.log('listening on port 30000');
+    var options = {
+        secure:true,
+        reconnect: true,
+        rejectUnauthorized : false
+    };
+
+    //socket.io-clientでサーバへ接続
+    var socket = client.connect('https://localhost:30000/namespace',options);
+
+    //処理
+    socket.on('connect',function(){
+        console.log('connect: https://localhost:30000/namespace');
+        //socket.send('how are you?');
+        //socket.disconnect();
+        //process.exit(0);
+    });
+
+    socket.on('connect_error',function(e){
+        console.log('connect_error e=' + e);
+    });
+
+    conn.on('data', function(data){
+        console.log('server-> ' + data + ' from ' + conn.remoteAddress + ':' + conn.remotePort);
+        conn.write('server -> Repeating: ' + data);
+    });
+    conn.on('close', function(){
+        console.log('server-> client closed connection');
+    });
+
+}).listen(30001);
+
+console.log('listening on port 30001');
