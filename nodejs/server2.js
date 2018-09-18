62406a7e2c2797528401d6726084deb628028bec
ikuratakashi <ikura1108@icloud.com>
2018-09-18 22:49:56 +0900

    a

--- nodejs/server2.js
+++ nodejs/server2.js
@@ -28,7 +28,7 @@ var server = net.createServer(function(conn){
 
     conn.on('data', function(data){
         console.log('server-> ' + data + ' from ' + conn.remoteAddress + ':' + conn.remotePort);
-        conn.write('server -> Repeating: ' + data);
+        //conn.write('server -> Repeating: ' + data);
     });
     conn.on('close', function(){
         console.log('server-> client closed connection');
