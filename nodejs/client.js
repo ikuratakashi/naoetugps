e1c58e622ff70ce866f0626df80cb173209b2723
ikuratakashi <ikura1108@icloud.com>
2018-09-18 22:30:12 +0900

    a

--- nodejs/client.js
+++ nodejs/client.js
@@ -1,14 +1,4 @@
-//設定ファイルの読み込み
-require('dotenv').config();
-
-var fs = require('fs');
-var ssl_server_key = '../../ssl/new_server.key';
-var ssl_server_crt = '../../ssl/server.crt';
 var options = {
-//    key: fs.readFileSync(ssl_server_key),
-//    cert: fs.readFileSync(ssl_server_crt),
-//    pfx: fs.readFileSync(ssl_server_crt),
-//    passphrase: process.env.HTTPS_PASS,
     secure:true,
     reconnect: true,
     rejectUnauthorized : false
@@ -16,13 +6,12 @@ var options = {
 
 //socket.io-clientでサーバへ接続
 var client = require('socket.io-client');
-var socket = client.connect('https://arukisoft.com:30000/namespace',options);
+var socket = client.connect('https://localhost:30000/namespace',options);
 
 //処理
 //connectしたら'how are you?'とメッセージを送信する
 socket.on('connect',function(){
-    console.log('yea!!');
-    socket.send('how are you2?');
+    socket.send('how are you?');
     //socket.disconnect();
     //process.exit(0);
 });
